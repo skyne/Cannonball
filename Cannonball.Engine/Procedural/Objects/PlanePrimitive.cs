4fb83968bac4ad1f1fe9666b7be405969a84256f
nagysa1313 <nagysa1313@gmail.com>
2014-05-07 15:30:10 +0200

    Added new vertex type (position+texture), started to implement billboarding

---
+++ Cannonball.Engine/Procedural/Objects/PlanePrimitive.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cannonball.Engine.Procedural.Objects
+{
+    public class PlanePrimitive : GeometricPrimitive
+    {
+        public PlanePrimitive(GraphicsDevice graphicsDevice, float size = 1)
+        {
+            var halfSize = size / 2;
+
+
+
+            InitializePrimitive(graphicsDevice);
+        }
+    }
+}
